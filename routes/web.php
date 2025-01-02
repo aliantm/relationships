c8565083ab898ef4087f92a98735b73a68181de1
aliantm <atfree1988@gmail.com>
2025-01-02 12:05:47 -0500

    one_to_one

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Models\Phone;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/prueba', function () {
+    /*User::create([
+        'name' => 'Alian Torne',
+        'email' => 'atfree1988@gmail.com',
+        'password' => bcrypt('12345678')
+    ]);
+    return 'Usuario creado';
+    Phone::create([
+        'number' => '987654321',
+        'user_id' => 1
+    ]);
+    //$user = User::find('1');
+    $user = User::where('id',1)
+                ->with('phone')
+                ->first();*/
+    $phone = Phone::find('1');
+    return $phone->user;
 });
